fa8b70633b03968a2c774f4cae6aa1e32a2a9e85
Adrika Bharti <adrikabharti@gmail.com>
2025-06-24 23:30:32 +0530

    final commit

--- controllers/refreshController.js
+++ controllers/refreshController.js
@@ -6,10 +6,10 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const handleRefreshToken = (req,res)=>{
     const cookies = req.cookies;
-    if(!cookies?.jwt) return res.sendSatus(401)
+    if(!cookies?.jwt) return res.sendStatus(401)
     const refreshToken = cookies.jwt
     const foundUser = usersDB.users.find(user=> user.refreshToken===refreshToken)
-    if(!foundUser) return res.sendSatus(403)
+    if(!foundUser) return res.sendStatus(403)
     
     const roles = Object.values(foundUser.roles)
     jwt.verify(
